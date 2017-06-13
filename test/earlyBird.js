3f784af55f6b12010a55786b3d2c64722ba0b287
bpeters <brennen.peters@gmail.com>
2017-06-13 17:05:26 +0100

    add test

--- test/earlyBird.js
+++ test/earlyBird.js
@@ -1,11 +1,14 @@
 var EarlyBird = artifacts.require("./EarlyBird.sol");
 
 contract('EarlyBird', (accounts) => {
-  it('fails to register sim without correct balance', async () => {
+  it('deployed parameters are correct', async () => {
     const contract = await EarlyBird.deployed();
+    const owner = await contract.owner();
+    const isLocked = await contract.isLocked();
 
-    console.log('EarlyBird', contract);
-    
+    assert.equal(owner, accounts[0]);
+    assert.equal(isLocked, false);
+
     return Promise.resolve();
   });
 });
--- test/passport.js
+++ test/passport.js
@@ -3,8 +3,6 @@ const Passport = artifacts.require("./Passport.sol");
 contract('Passport', (accounts) => {
   it('fails to register sim without correct balance', async () => {
     const contract = await Passport.deployed();
-
-    console.log('PASSPORT', contract);
     
     return Promise.resolve();
   });
