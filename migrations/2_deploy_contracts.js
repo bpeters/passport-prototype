fbdd5d1d57f0f1ccf34e1b149cdb73eb614ad5fe
bpeters <brennen.peters@gmail.com>
2017-06-13 16:02:36 +0100

    update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Passport = artifacts.require("./Passport.sol");
+var EarlyBird = artifacts.require("./EarlyBird.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Passport);
+  deployer.deploy(EarlyBird);
 };
